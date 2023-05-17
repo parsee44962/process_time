f71ee6dcbd02adb23eec049f3570d36160cb82ca
parsee44962 <vv33555@163.com>
2023-05-17 14:32:14 +0800

    checked

--- 1.py
+++ 1.py
@@ -2,15 +2,17 @@ import os
 import csv
 
 data = []
-with open("runtime.log","r") as f:
+with open("gpu_runtime.log","r") as f:
     for line in f.readlines():
-        line = line.strip()
-        soup = line.split('.')[1]
-        name = soup.split("=")[0].strip()
-        time = soup.split('=')[1].strip()
-        data.append({'name':name,'time':time})
+        if "Vina-GPU total runtime =" in line:
+            break
+        else:
+            line = line.strip()
+            name = os.path.basename(line.split(" ")[6])
+            time = line.split(" ")[8]
+            data.append({'name':name,'time':time})
 
-f = open('test1.csv','a',encoding='utf8',newline='')  # 指定newline=‘’参数
+f = open('test1.csv','w',encoding='utf8',newline='')  # 指定newline=‘’参数
 writer = csv.DictWriter(f,fieldnames=['name','time'])
 writer.writeheader() # 将字段写入csv格式文件首行
 for line in data:
